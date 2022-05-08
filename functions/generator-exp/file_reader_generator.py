56996061338ca60dff47025a942245b873cf0079
Roy <roy@jan.co.il>
2022-05-08 17:49:16 +0300

    added docstring

--- functions/generator-exp/file_reader_generator.py
+++ functions/generator-exp/file_reader_generator.py
@@ -8,6 +8,11 @@ def read_without_generator(_file: BufferedRandom):
 
 
 def read_with_generator(_file: BufferedRandom, chunk_size: int = 1024):
+    """
+    :param _file: file object
+    :param chunk_size: size of chunk -> integer
+    :return: next chunk data or break loop
+    """
     while True:
         data = _file.read(chunk_size)
         if not data:
