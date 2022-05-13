8651552eb998fc25e3110cf8690f0c6c59d49d89
Roy <roy@jan.co.il>
2022-05-13 11:19:39 +0300

    added files

--- misc/args-parser-example/args-parser.py
+++ misc/args-parser-example/args-parser.py
@@ -18,14 +18,7 @@ def timing(f):  # https://stackoverflow.com/questions/1622943/timeit-versus-timi
     return wrap
 
 
-def get_arguments():
-    parser = argparse.ArgumentParser(description='Get file duration')
-    parser.add_argument("-m", "--media", type=str, required=True, help="Media path")
-    parser.add_argument("-ti", "--timeit", required=False, help="How much time it took", action="store_true")
-    return parser.parse_args()
-
-
-def get_file_duration(file_path: str):
+def get_file_duration(file_path: str):  # https://stackoverflow.com/questions/7833807/get-wav-file-length-or-duration
     with contextlib.closing(wave.open(file_path)) as file:
         frames = file.getnframes()
         rate = file.getframerate()
@@ -33,8 +26,15 @@ def get_file_duration(file_path: str):
     return duration
 
 
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Get file duration')
+    parser.add_argument("-m", "--media", required=True, help="Media path", type=str)
+    parser.add_argument("-ti", "--timeit", required=False, help="Time to run", action="store_true")
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
     my_args = get_arguments()
-    function = get_file_duration if not my_args.timeit else timing(get_file_duration)
+    function = timing(get_file_duration) if my_args.timeit else get_file_duration
     duration_file = function(my_args.media)
-    print(duration_file)
+    print(f"{my_args.media} - {duration_file} seconds")
